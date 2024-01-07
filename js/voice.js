fb4249e3793e0f1d171a1b105c8aef3dbe904844
osama764 <146546427+osama764@users.noreply.github.com>
2024-01-07 16:45:28 +0200

    Update voice.js

--- js/voice.js
+++ js/voice.js
@@ -82,14 +82,16 @@ recognition.onresult = async function (event) {
 }
 
 if (words[0] == "شغل" && words.length > 1) {
+  
+  recognition.stop();
   const surahName = words.slice(1).join(' ');
   await speak("جاري تنفيذ طلبك");
   searchAndPlaySurahOnYouTube(surahName);
+
 } 
 if (words[0] == "وقف" || words[0] == "اسكت") {
   stopYouTubePlayer();
 } 
-
 
 
 
@@ -293,63 +295,68 @@ async function speak(text) {
 
 // ************************* اوامر اضافية******************
 
+
 let player;
 
 function searchAndPlaySurahOnYouTube(surahName) {
-  const apiKey = 'AIzaSyB6-iYMYCniXT5xCoKOV6JH2KLLTRCWado'; // يجب وضع مفتاح API الخاص بك هنا
+  const apiKey = 'AIzaSyB6-iYMYCniXT5xCoKOV6JH2KLLTRCWado';
   const apiUrl = `https://www.googleapis.com/youtube/v3/search?q=${surahName}&part=snippet&type=video&key=${apiKey}`;
 
   // قبل تشغيل الفيديو الجديد، تأكد من إيقاف أي فيديو قيد التشغيل
-  if(player){
 
-    stopYouTubePlayer();
-  }
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => {
+        if (data.items && data.items.length > 0) {
+          const videoId = data.items[0].id.videoId;
 
-  fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => {
-      const videoId = data.items[0].id.videoId;
-      
-      // قم بتشغيل الفيديو الجديد
-      player = new YT.Player('youtube-player', {
-        height: '0',
-        width: '0',
-        videoId: videoId,
-        playerVars: {
-          'autoplay': 1,
-          'controls': 0,
-          'showinfo': 0,
-          'modestbranding': 1,
-          'loop': 1,
-          'fs': 0,
-          'cc_load_policy': 0,
-          'iv_load_policy': 3,
-        },
-        events: {
-          'onReady': onPlayerReady,
-        },
+          // قم بتشغيل الفيديو الجديد
+          player = new YT.Player('youtube-player', {
+            height: '0',
+            width: '0',
+            videoId: videoId,
+            playerVars: {
+              'autoplay': 1,
+              'controls': 0,
+              'showinfo': 0,
+              'modestbranding': 1,
+              'loop': 1,
+              'fs': 0,
+              'cc_load_policy': 0,
+              'iv_load_policy': 3,
+            },
+            events: {
+              'onReady': onPlayerReady,
+            },
+          });
+        } else {
+          console.error('لم يتم العثور على فيديوهات.');
+        }
+      })
+      .catch(error => {
+        console.error('حدث خطأ أثناء البحث عن السورة على YouTube:', error);
       });
-    })
-    .catch(error => {
-      console.error('حدث خطأ أثناء البحث عن السورة على YouTube:', error);
-    });
+  
 }
 
 function onPlayerReady(event) {
+  console.log('تم تشغيل الفيديو');
   event.target.playVideo();
+  // يمكنك إضافة رسالة إلى المستخدم هنا
 }
 
 function stopYouTubePlayer() {
   // إذا كان هناك فيديو قيد التشغيل وليس محذوفًا، قم بإيقافه
   if (player && player.stopVideo) {
+    console.log('تم إيقاف الفيديو');
     player.stopVideo();
-    player.destroy(); // قطع الاتصال مع المشغل السابق
+    if (player.destroy) {
+      console.log('تم قطع الاتصال مع المشغل السابق');
+      player.destroy(); // قطع الاتصال مع المشغل السابق
+    }
+    // يمكنك إضافة رسالة إلى المستخدم هنا
   }
 }
-
-
-
-
 
 
 
